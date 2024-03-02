10bfba1b1e2863688f78903758721456d9f873d8
João Victor <111701155+jvpcms@users.noreply.github.com>
2024-03-02 17:28:01 -0300

    Update Binary_Search_Tree_Insertion.cpp

--- Binary_Search_Tree_Insertion.cpp
+++ Binary_Search_Tree_Insertion.cpp
@@ -30,23 +30,6 @@ class Solution {
       	preOrder(root->right);
     }
 
-/*
-Node is defined as 
-
-class Node {
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d) {
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
-
-*/
-
     Node * insert(Node * root, int data) {
         
         if (root == NULL) return new Node(data);

172ce83fcfe128a0b8993edd653ed950a6ab0b5c
João Victor <111701155+jvpcms@users.noreply.github.com>
2024-03-03 17:50:15 -0300

    Create Knapsack_Problem.cpp
    A collection of itens, each with varying "value" and "weight."
    Find out the maximum value that can be obtained in a subset of itens, such that the total weight does not surpass a given maximum.

---
+++ Knapsack_Problem.cpp
@@ -0,0 +1,33 @@
+// by jvpcms
+
+/* A collection of itens, each with varying "value" and "weight."
+Find out the maximum value that can be obtained in a subset of itens,
+such that the total weight does not surpass a given maximum. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2005;
+int weight[N], value[N];
+
+int dp[N][N];
+
+int main(){
+  
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int s, n; cin >> s >> n;
+
+    for (int i = 1; i <= n; i++) cin >> weight[i] >> value[i];
+
+    for (int i = 1; i <= n; i++) for (int w = 0; w <= s; w++)
+    {
+        dp[i][w] = dp[i - 1][w];
+        if (w - weight[i] >= 0) dp[i][w] = max(dp[i][w], dp[i - 1][w - weight[i]] + value[i]);
+    }
+
+    cout << dp[n][s] << '\n';
+
+    return 0;
+}
